91d71a051c2b15060790467f5f0cf55ba0f5a80e
Trevor McNamara <trevormcnamara@email.arizona.edu>
2020-06-10 22:56:48 -0700

    please

---
+++ questions.js
@@ -0,0 +1,56 @@
+var jsQuestions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "booleans"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Arrays in Javascipt can be used to store ______",
+    choices: ["Numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+  },
+  {
+    title: "String values must be enclosed within _____ when being assigned to variables",
+    choices: ["Commas", "Curly brackets", "quotes", "parentheses"],
+    answer: "parentheses"
+  },
+  {
+    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["Javascript", "terminal/bash", "for loops", "console log"],
+    answer: "for loops"
+  },
+
+];
+
+var javascriptQuestions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "booleans"
+      },
+      {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+      },
+      {
+        title: "Arrays in Javascipt can be used to store ______",
+        choices: ["Numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "All of the above"
+      },
+      {
+        title: "String values must be encolsed within _____ when being assigned to variables",
+        choices: ["Commas", "Curly brackets", "quotes", "parentheses"],
+        answer: "parentheses"
+      },
+      {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal/bash", "for loops", "console log"],
+        answer: "for loops"
+      },
+];
